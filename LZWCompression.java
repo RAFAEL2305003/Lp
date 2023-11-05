ed4002ff3dab54a95ce4727ad4aba7ea11072227
RAFAEL2305003 <114117847+RAFAEL2305003@users.noreply.github.com>
2023-11-05 16:10:34 -0300

    Add files via upload

--- LZWCompression.java
+++ LZWCompression.java
@@ -1,95 +1,91 @@
-import java.util.Scanner;
+import java.util.*;
 
-class Dicionario {
-  public String[] table;
+public class LZWCompression {
+    static Map<Integer, String> MapDecompress = new HashMap<>();
+    
+    public static String compress(String input) {
+        Map<String, Integer> MapCompress = new HashMap<>();
 
-  public Dicionario() {
-    table = new String[256];
+        int j = 0, code = 0, x = 0;
+        String result = "";
 
-    int i;
-    for (i = 0; i < table.length; i++)
-      table[i] = null;
-  }
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(!MapCompress.containsKey(String.valueOf(c))) {
+                MapCompress.put(String.valueOf(c), j++);
+                MapDecompress.put(x++ ,String.valueOf(c));
+            }
+        }
 
-  public boolean exists(char c) {
-    int i;
-    boolean found = false;
+        String p = String.valueOf(input.charAt(0));
+        j = 1;
 
-    for (i = 0; i < table.length; i++) {
-      if (table[i] != null && table[i].charAt(0) == c) {
-        found = true;
-      }
+        while(j < input.length()) {
+            String c = String.valueOf(input.charAt(j));
+            String pc = p + c;
+            if(MapCompress.containsKey(String.valueOf(p+c))) {
+                p = pc;
+            } else {
+                code = MapCompress.get(p);
+                result += code + "";
+                MapCompress.put(pc, MapCompress.size());
+                p = String.valueOf(c);
+            }
+            j++;
+        }
+
+        int codeLast = MapCompress.get(p);
+        result += codeLast + "";
+
+        return result;
     }
 
-    return found;
-  }
+    public static String decompress(String compressedText) {
+        String result = "", s = "", c = "";
+        int tmp = 0, i = 0;
 
-  public int pos(char c) {
-    int i = 0;
+        int code = Character.getNumericValue(compressedText.charAt(i));
+        i++;
 
-    for (i = 0; i < table.length; i++) {
-      if (table[i] != null && table[i].charAt(0) == c) {
-        break;
-      }
+        s = MapDecompress.get(code);
+        result += s;
+
+        while (i < compressedText.length()) {
+            code = Character.getNumericValue(compressedText.charAt(i));
+            i++;
+
+            if (!MapDecompress.containsKey(code)) {
+                tmp = Character.getNumericValue(compressedText.charAt(i - 1));
+                String old = MapDecompress.get(tmp);
+                s = old + c;
+            } else {
+                s = MapDecompress.get(code);
+            }
+
+            result += s;
+            c = String.valueOf(s.charAt(0));
+            MapDecompress.put(MapDecompress.size(), MapDecompress.get(Character.getNumericValue(compressedText.charAt(i - 2))) + c);
+        }
+
+        return result;
     }
+    
 
-    return i;
-  }
-
-  public void preencherTabela(String str) {
-    int index = 0;
-    for (int i = 0; i < str.length(); i++) {
-      if (table[index] == null && !exists(str.charAt(i))) {
-        table[index] = str.charAt(i) + "";
-        index++;
-      }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Your input: ");
+        String input = sc.nextLine();
+        System.out.println();
+        String result = compress(input);
+        
+        char[] str = result.toCharArray();
+        System.out.print("Compressed text: ");
+        
+        for(int i = 0; i < str.length; i++) 
+            System.out.print(str[i] + " ");    
+        System.out.println();
+        
+        System.out.println("Descompressed text: " + decompress(result));
     }
-  }
-
-  public void printarTabela() {
-    int i;
-    for (i = 0; i < table.length; i++) {
-      if (table[i] != null) {
-        System.out.println(i + " " + table[i]);
-      }
-    }
-  }
-
-  public boolean search(char c) {
-    boolean found = false;
-    int i;
-    for (i = 0; i < table.length; i++) {
-      if (table[i] != null && table[i].equals(c + "")) {
-        found = true;
-      }
-    }
-
-    return found;
-  }
-
-  public void aux(String aux) {
-    int i;
-    System.out.println();
-    for (i = 0; i < aux.length(); i++) {
-      if (search(aux.charAt(i))) {
-        String binario = Integer.toBinaryString(pos(aux.charAt(i)));
-        System.out.print(binario + " ");
-      }
-    }
-    System.out.println();
-  }
-}
-
-public class Lzw {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    Dicionario d = new Dicionario();
-    System.out.println("Digite a frase a ser comprimida: ");
-    String input = sc.nextLine();
-    d.preencherTabela(input);
-    d.printarTabela();
-    d.aux(input);
-
-    sc.close();
-  }
 }
